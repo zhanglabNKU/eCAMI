1751082409ab789ad3eae82bd438151389b7d869
joy1314 <782761855@qq.com>
2019-09-15 23:35:01 +0800

    edit 2

--- prediction.py
+++ prediction.py
@@ -54,27 +54,22 @@ def find_all(sub,s):
 		return -1
 
 
-
-def get_cluster_number(file_name,n_mer_dir_path,output_dir,n_mer,piece_number,protein_name,protein_string,important_n_mer_number,beta):
-
+def get_kmer_dict(n_mer_dir_path):
     kmer_dir_list=os.listdir(n_mer_dir_path)
-
-    for i in range(len(kmer_dir_list)):
-        kmer_dir_list[i]=kmer_dir_list[i]+'/kmer_for_each_cluster'
-    dir_name=n_mer_dir_path
-
-    kmer_label=[]
-    kmer_message=[]
-    all_cluster_kmer=[]
-    for each_kmer_dir in kmer_dir_list:
-        kmer_file_list=os.listdir(dir_name+'/'+each_kmer_dir)
+    fam_kmer_dict={}
+    for k in range(len(kmer_dir_list)):
+        dir_name=n_mer_dir_path
+        kmer_label=[]
+        kmer_message=[]
+        all_cluster_kmer=[]
+        kmer_file_list=os.listdir(dir_name+'/'+kmer_dir_list[k]+'/kmer_for_each_cluster')
         for each_file in kmer_file_list:
-            f=open(dir_name+'/'+each_kmer_dir+'/'+each_file,'r')
+            f=open(dir_name+'/'+kmer_dir_list[k]+'/kmer_for_each_cluster'+'/'+each_file,'r')
             text=f.read()
             text=text.split('\n')
             f.close()
-            kmer_label.append(each_kmer_dir.split('/')[0]+':'+text[0].split(':')[1])
-
+            kmer_label.append(kmer_dir_list[k]+':'+text[0].split(':')[1])
+            temp_name=((text[1].split('\t')[0]).split(':')[0]).split('_')[0]
             kmer_message.append(text[1].split('\t')[1])
             each_cluster_kmer=text[2]
             each_cluster_kmer=each_cluster_kmer.split('\t')
@@ -82,62 +77,115 @@ def get_cluster_number(file_name,n_mer_dir_path,output_dir,n_mer,piece_number,pr
                 each_cluster_kmer.remove('')
             cluster_kmer={}
             for each_kmer in each_cluster_kmer:
-                cluster_kmer[each_kmer.split(':')[0]]=float(each_kmer.split(':')[1])
+                temp_mess=each_kmer.split(':')
+                cluster_kmer[temp_mess[0]]=float(temp_mess[1])
             all_cluster_kmer.append(cluster_kmer)
-    f=open(output_dir+file_name,'w')
+
+        temp_name=temp_name.split('.')
+        while '-' in temp_name:
+            temp_name.remove('-')
+        temp_name='.'.join(temp_name)
+
+        fam_kmer_dict[temp_name]=[kmer_label,kmer_message,all_cluster_kmer]
+    return fam_kmer_dict
+
+def write_line(selected_fam_message,temp_protein_string):
+    string_line=''
+    string_line+=str(selected_fam_message[4])+'\t'+str(selected_fam_message[3])+'\n'
+    temp_same_kmer=selected_fam_message[2]
+    kmer_index_dict={}
+    for each_kmer in temp_same_kmer:
+        index=find_all(each_kmer,temp_protein_string)
+        kmer_index_dict[np.min(index)]=[each_kmer,index]
+    sorted_kmer_index_dict=sorted(kmer_index_dict.items(),key=lambda x:x[0])
+    for each_member in sorted_kmer_index_dict:
+        string_line+=each_member[1][0]
+        string_line+='('
+        index=each_member[1][1]
+        for m in range(len(index)):
+            if m==len(index)-1:
+                string_line+=str(index[m])
+            else:
+                string_line+=str(index[m])+','
+        string_line+=')'+','
+    string_line+='\n'
+    return string_line
+
+def get_cluster_number(file_name,fam_kmer_dict,output_dir,n_mer,piece_number,protein_name,protein_string,important_n_mer_number,beta):
+
+
+    fw=open(output_dir+file_name,'w')
     for i in piece_number:
-#        print(i)
         kmer=[]
         for j in range(n_mer):
             text=protein_string[i][j:len(protein_string[i])]
             kmer+=re.findall('.{'+str(n_mer)+'}', text)
         kmer=list(set(kmer))
-        each_cluster_score=[]
-        each_cluster_number_score=[]
-        each_same_kmer=[]
-        for j in range(len(all_cluster_kmer)):
-            score=0
-            number_score=0
-            each_cluster_kmer=all_cluster_kmer[j]
-            same_kmer=list(set(kmer).intersection(set(each_cluster_kmer.keys())))
-            score+=len(same_kmer)
-            if score>=important_n_mer_number:
-                for k in same_kmer:
+        selected_fam_dict={}
+        sort_fam=[]
+        for each_fam in fam_kmer_dict.keys():
+            all_cluster_kmer=fam_kmer_dict[each_fam][2]
+            kmer_message=fam_kmer_dict[each_fam][1]
+            kmer_label=fam_kmer_dict[each_fam][0]
+            each_cluster_score=[]
+            each_cluster_number_score=[]
+            each_same_kmer=[]
+            for j in range(len(all_cluster_kmer)):
+                score=0
+                number_score=0
+                each_cluster_kmer=all_cluster_kmer[j]
+                same_kmer=list(set(kmer).intersection(set(each_cluster_kmer.keys())))
+                temp_same_kmer=same_kmer[:]
+                for k in temp_same_kmer:
                     number_score+=each_cluster_kmer[k]
-            each_cluster_score.append(score)
-            each_cluster_number_score.append(number_score)
-            each_same_kmer.append(same_kmer)
-        max_similary_cluster_label=np.argmax(each_cluster_score)
-#        py = psutil.Process(os.getpid())
-#        print(str(round(py.memory_info()[0]/2.**30,2))+':GB')
+                score+=len(same_kmer)
 
-        if each_cluster_number_score[max_similary_cluster_label]>=beta and each_cluster_score[max_similary_cluster_label]>=important_n_mer_number:
-
-            f.write(protein_name[i]+'\t'+str(kmer_label[max_similary_cluster_label])+'\t'+str(kmer_message[max_similary_cluster_label])+'\n')
-            kmer_index_dict={}
-            for each_kmer in each_same_kmer[max_similary_cluster_label]:
-                index=find_all(each_kmer,protein_string[i])
-                kmer_index_dict[np.min(index)]=[each_kmer,index]
-            sorted_kmer_index_dict=sorted(kmer_index_dict.items(),key=lambda x:x[0])
-            for each_member in sorted_kmer_index_dict:
-                f.write(each_member[1][0])
-                f.write('(')
-                index=each_member[1][1]
-                for m in range(len(index)):
-                    if m==len(index)-1:
-                        f.write(str(index[m]))
-                    else:
-                        f.write(str(index[m])+',')
-                f.write(')'+',')
-            f.write('\n')
-    f.close()
+                each_cluster_score.append(score)
+                each_cluster_number_score.append(number_score)
+                each_same_kmer.append(same_kmer)
+            max_similary_cluster_label=np.argmax(each_cluster_score)
+            if each_cluster_number_score[max_similary_cluster_label]>=beta and each_cluster_score[max_similary_cluster_label]>=important_n_mer_number:
+                sort_fam.append([each_cluster_score[max_similary_cluster_label],each_cluster_number_score[max_similary_cluster_label],each_fam])
+                selected_fam_dict[each_fam]=[each_cluster_score[max_similary_cluster_label],\
+                                  each_cluster_number_score[max_similary_cluster_label],each_same_kmer[max_similary_cluster_label],\
+                                  kmer_message[max_similary_cluster_label],kmer_label[max_similary_cluster_label]]
+        if len(sort_fam)==0:
+            continue
+        sort_fam=sorted(sort_fam,key=(lambda x:x[1]),reverse=True)
+        sort_fam=sorted(sort_fam,key=(lambda x:x[0]),reverse=True)
 
 
+        first_fam=sort_fam[0][2]
+        fam_key=list(selected_fam_dict.keys())
+        fam_key.remove(first_fam)
+        for each_fam_key in fam_key:
+            if len(set(list(''.join(selected_fam_dict[each_fam_key][2]))))<5:
+                del selected_fam_dict[each_fam_key]
+        flag=len(first_fam.split('.'))
+        fw.write(protein_name[i]+'\t'+write_line(selected_fam_dict[first_fam],protein_string[i]))
+        used_fam=[first_fam]
+        other_fams=selected_fam_dict[first_fam][3]
+        other_fams=other_fams.split('|')
+        for each_fam in other_fams:
+            temp_fam=each_fam.split(':')
+            temp_name=temp_fam[0].split('_')[0]
+            if flag>1:
+                temp_name=temp_name.split('.')
+                temp_name='.'.join(temp_name[0:flag])
+            if len(temp_fam)==2 and temp_name not in used_fam:
+                used_fam.append(temp_fam[0].split('_')[0])
+                if int(temp_fam[1])>1 and temp_name in selected_fam_dict.keys():
+                    fw.write(protein_name[i]+'\t'+write_line(selected_fam_dict[temp_name],protein_string[i]))
+                    
+
+    fw.close()
 
 
 
 
-def get_validation_results(input_fasta_file,database_dir,output_dir,output_file_name,n_mer,important_n_mer_number,beta,n_mer_dir_path,jobs):
+
+
+def get_validation_results(input_fasta_file,database_dir,output_dir,output_file_name,n_mer,important_n_mer_number,beta,fam_kmer_dict,jobs):
     if database_dir:
         database_dir=database_dir+'/'
     [protein_name,protein_string] = read_file(database_dir,input_fasta_file)
@@ -167,9 +215,9 @@ def get_validation_results(input_fasta_file,database_dir,output_dir,output_file_
 
     for i in range(jobs):
         file_name=input_fasta_file+'_thread_'+str(i)+'.txt'
-#        get_cluster_number(file_name,n_mer_dir_path,n_mer_dir_name,temp_output_dir,n_mer,piece_number[i],protein_name,protein_string,important_n_mer_number,beta)
+#        get_cluster_number(file_name,fam_kmer_dict,temp_output_dir,n_mer,piece_number[i],protein_name,protein_string,important_n_mer_number,beta)
 
-        pool.apply_async(get_cluster_number, (file_name,n_mer_dir_path,temp_output_dir,n_mer,piece_number[i],protein_name,protein_string,important_n_mer_number,beta,))
+        pool.apply_async(get_cluster_number, (file_name,fam_kmer_dict,temp_output_dir,n_mer,piece_number[i],protein_name,protein_string,important_n_mer_number,beta,))
     pool.close()
     pool.join()
     fw=open(temp_output_dir+pred_file_name,'w')
@@ -201,8 +249,8 @@ def arg_parser(args):
     parser.add_argument('-input',     default="examples/prediction/input/test.faa",type=str,        help="Define the fasta file name")
     parser.add_argument('-k_mer',           default=8,         type=int,                 help="Peptide length for prediction")
     parser.add_argument('-jobs',            default=8,         type=int,                 help='Number of processor for use for prediction')
-    parser.add_argument('-important_k_mer_number',        default=3,      type=int,               help="Minimum number of n_mer for prediction")
-    parser.add_argument('-beta',        default=1,      type=float,               help="Minimum sum of percentage of frequency of n_mer for prediction")
+    parser.add_argument('-important_k_mer_number',        default=5,      type=int,               help="Minimum number of n_mer for prediction")
+    parser.add_argument('-beta',        default=2,      type=float,               help="Minimum sum of percentage of frequency of n_mer for prediction")
 
 
     args = parser.parse_args(args)
@@ -244,7 +292,8 @@ if __name__ == "__main__":
     beta=args.beta
 
     print(input_fasta_file)
-    get_validation_results(input_fasta_file,database_dir,output_dir,output_file_name,n_mer,important_n_mer_number,beta,n_mer_dir_path,jobs)
+    fam_kmer_dict=get_kmer_dict(n_mer_dir_path)
+    get_validation_results(input_fasta_file,database_dir,output_dir,output_file_name,n_mer,important_n_mer_number,beta,fam_kmer_dict,jobs)
 
     endtime = datetime.datetime.now()
     print('total time:'+str((endtime - starttime).total_seconds())+'s')
